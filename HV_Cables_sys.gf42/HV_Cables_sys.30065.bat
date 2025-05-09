dd5ba9f882a1de069ef880e73c8681a635e7a88b
lmw <131606253+xsjiasnxj@users.noreply.github.com>
2025-05-09 14:15:01 +0800

    v1.0.0 （first commit）

---
+++ HV_Cables_sys.gf42/HV_Cables_sys.30065.bat
@@ -0,0 +1,4 @@
+call "F:\PSCAD\gfortran\bin\gf42vars.bat"
+pushd "D:\liumingwei\Documents\PSCAD\Project_0417\HV_Cables_sys.gf42\"
+HV_Cables_sys.exe -v4 192.168.232.1 30065 locale=english-us
+popd
---
+++ HV_Cables_sys.gf42/HV_Cables_sys.mak.bat
@@ -0,0 +1,4 @@
+call "F:\PSCAD\gfortran\bin\gf42vars.bat"
+pushd "D:\liumingwei\Documents\PSCAD\Project_0417\HV_Cables_sys.gf42\"
+make -f HV_Cables_sys.mak
+popd
